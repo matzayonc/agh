2bf225f5981876db2bf5041b31e8cf410c086368
Mateusz Zając <matzayonc@gmail.com>
2022-04-22 22:13:56 +0200

    done

--- asd/offline4/zad4.py
+++ asd/offline4/zad4.py
@@ -1,22 +1,16 @@
 from zad4testy import runtests
 
-
-def value(T, S):
-    s = 0
-    for i in S:
-        s += (T[i][2] - T[i][1]) * T[i][0]
-
-    return s
+M = {}
 
 
 def collides(T, i, j):
     return T[i][2] >= T[j][1] and T[i][1] <= T[j][2]
 
 
-def cache(func, M={}):
+def cache(func):
     def wrapper(*args):
 
-        k = str(args[1]) + '-' + str(args[2]) + '-' + str(args[0])
+        k = str(args[1]) + '-' + str(args[2])
 
         if k not in M:
             M[k] = func(*args)
@@ -33,18 +27,18 @@ def select(T, p, i):
         if collides(T, i, k) or p - T[i][3] < T[k][3]:
             continue
         c = select(T, p - T[i][3], k)
-        c = (value(T, c), c)
 
         if c[0] > m[0]:
             m = c
 
-    return [i, *m[1]]
+    return (m[0] + T[i][0], [i, *m[1]])
 
 
 def select_buildings(T, p):
-
+    global M
+    M = {}
     for i in range(len(T)):
-        T[i] = (*T[i], i)
+        T[i] = ((T[i][2] - T[i][1]) * T[i][0], *T[i][1:4], i)
 
     T.sort(key=lambda x: x[1])
 
@@ -54,7 +48,6 @@ def select_buildings(T, p):
             continue
 
         c = select(T, p, k)
-        c = (value(T, c), c)
 
         if c[0] > m[0]:
             m = c
