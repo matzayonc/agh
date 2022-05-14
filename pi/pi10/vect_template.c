6ab746b416ac59f2202710dfc2592ee8956a707c
Mateusz Zając <matzayonc@gmail.com>
2022-05-15 00:36:46 +0200

    first fine

--- pi/pi10/vect_template.c
+++ pi/pi10/vect_template.c
@@ -4,14 +4,16 @@
 #include <ctype.h>
 #define MAX_STR_LEN 64
 
-typedef struct Vector {
+typedef struct Vector
+{
 	void *data;
 	size_t element_size;
 	size_t size;
 	size_t capacity;
 } Vector;
 
-typedef struct Person {
+typedef struct Person
+{
 	int age;
 	char first_name[MAX_STR_LEN];
 	char last_name[MAX_STR_LEN];
@@ -19,7 +21,8 @@ typedef struct Person {
 
 // Allocate vector to initial capacity (block_size elements),
 // Set element_size, size (to 0), capacity
-void init_vector(Vector *vector, size_t block_size, size_t element_size){
+void init_vector(Vector *vector, size_t block_size, size_t element_size)
+{
 	vector->element_size = element_size;
 	vector->size = 0;
 	vector->capacity = block_size;
@@ -28,11 +31,13 @@ void init_vector(Vector *vector, size_t block_size, size_t element_size){
 
 // If new_capacity is greater than the current capacity,
 // new storage is allocated, otherwise the function does nothing.
-void reserve(Vector *vector, size_t new_capacity){
-	if(vector->capacity >= new_capacity) return;
+void reserve(Vector *vector, size_t new_capacity)
+{
+	if (vector->capacity >= new_capacity)
+		return;
 
-	void* new_data = malloc(vector->element_size * new_capacity);
-	memcpy(new_data, vector->data, vector->capacity);
+	vector->data = realloc(vector->data, new_capacity * vector->element_size);
+	vector->capacity = new_capacity;
 }
 
 // Resizes the vector to contain new_size elements.
@@ -41,170 +46,314 @@ void reserve(Vector *vector, size_t new_capacity){
 
 // If the current size is less than new_size,
 // additional zero-initialized elements are appended
-void resize(Vector *vector, size_t new_size){
-	if(vector->size >= new_size)
-		vector->size = new_size;
-	else {
-		void* beggining_of_empty = vector->data + (vector->size * vector->element_size);
-		void* after_empty = vector->data + (new_size * vector->element_size);
+void resize(Vector *vector, size_t new_size)
+{
+	reserve(vector, new_size);
 
-		for (char* i = beggining_of_empty; i < after_empty; i += 1)
-			*i = 0;
+	if (vector->size >= new_size)
+	{
+		vector->size = new_size;
+		return;
 	}
+
+	for (size_t i = vector->size; i < new_size; i++)
+		for (size_t j = 0; j < vector->element_size; j++)
+		{
+			char *el = (char *)(vector->data + (i * vector->element_size) + j);
+			*el = 0;
+		}
+	vector->size = new_size;
 }
 
 // Add element to the end of the vector
-void push_back(Vector *vector, void *value){
-	void* el = vector->data + (vector->size * vector->element_size);
-	memcpy(new_data, vector->data, vector->capacity);
+void push_back(Vector *vector, void *value)
+{
+	reserve(vector, vector->size + 1);
+
+	void *el = vector->data + (vector->size * vector->element_size);
+	memcpy(el, value, vector->element_size);
+
+	vector->size += 1;
 }
 
 // Remove all elements from the vector
-void clear(Vector *vector);
+void clear(Vector *vector)
+{
+	vector->size = 0;
+}
 
 // Remove the last element from the vector
-void pop_back(Vector *vector);
-
+void pop_back(Vector *vector)
+{
+	vector->size -= 1;
+}
 // Insert new element at index (0 <= index <= size) position
-void insert(Vector *vector, int index, void *value);
+void insert(Vector *vector, int index, void *value)
+{
+	reserve(vector, vector->size + 1);
+	vector->size += 1;
+
+	for (int i = vector->size - 2; i >= index; i--)
+	{
+		void *el = vector->data + (i * vector->element_size);
+		memcpy(el + vector->element_size, el, vector->element_size);
+	}
+
+	void *el = vector->data + (index * vector->element_size);
+	memcpy(el, value, vector->element_size);
+}
 
 // Erase element at position index
-void erase(Vector *vector, int index);
+void erase(Vector *vector, int index)
+{
+	for (size_t i = index; i < vector->size - 1; i++)
+	{
+		void *el = vector->data + (i * vector->element_size);
+		memcpy(el, el + vector->element_size, vector->element_size);
+	}
+
+	vector->size -= 1;
+}
 
 // Erase all elements that compare equal to value from the container
-void erase_value(Vector *vector, void *value, int(*cmp)(const void*, const void*));
+void erase_value(Vector *vector, void *value, int (*cmp)(const void *, const void *))
+{
+	for (int i = 0; i < vector->size; i++)
+	{
+		void *el = vector->data + (i * vector->element_size);
+		if (cmp(el, value) == 0)
+		{
+			erase(vector, i);
+			i--;
+		}
+	}
+}
 
 // Erase all elements that satisfy the predicate from the vector
-void erase_if(Vector *vector, int (*predicate)(void *));
+void erase_if(Vector *vector, int (*predicate)(void *))
+{
+	for (int i = 0; i < vector->size; i++)
+	{
+		void *el = vector->data + (i * vector->element_size);
+		if (predicate(el))
+		{
+			erase(vector, i);
+			i--;
+		}
+	}
+}
 
 // Request the removal of unused capacity
-void shrink_to_fit(Vector *vector);
+void shrink_to_fit(Vector *vector)
+{
+	vector->data = realloc(vector->data, vector->size * vector->element_size);
+	vector->capacity = vector->size;
+}
 
 // Print integer vector
-void print_vector_int(Vector *vector);
+void print_vector_int(Vector *vector)
+{
+	printf("%zu\n", vector->size);
+
+	for (int i = 0; i < vector->size; i++)
+	{
+		int *el = (int *)(vector->data + (i * vector->element_size));
+		printf("%d ", *el);
+	}
+}
 
 // Print char vector
-void print_vector_char(Vector *vector);
+void print_vector_char(Vector *vector)
+{
+	printf("%zu\n", vector->size);
+
+	for (int i = 0; i < vector->size; i++)
+	{
+		char *el = vector->data + (i * vector->element_size);
+		printf("%c ", *el);
+	}
+}
 
 // Print vector of Person
-void print_vector_person(Vector *vector);
+void print_vector_person(Vector *vector)
+{
+	printf("%zu\n", vector->size);
+
+	for (int i = 0; i < vector->size; i++)
+	{
+		Person *el = vector->data + (i * vector->element_size);
+		printf("%i %s %s\n", el->age, el->first_name, el->last_name);
+	}
+}
 
 // integer comparator - increasing order
-int int_cmp(const void *v1, const void *v2);
+int int_cmp(const void *v1, const void *v2)
+{
+	int *a = (int *)v1;
+	int *b = (int *)v2;
+	return *a - *b;
+}
 
 // char comparator - lexicographical order (case sensitive)
-int char_cmp(const void *v1, const void *v2);
+int char_cmp(const void *v1, const void *v2)
+{
+	char *a = (char *)v1;
+	char *b = (char *)v2;
+	return *a - *b;
+}
 
 // Person comparator:
 // Sort according to age (decreasing)
 // When ages equal compare first name and then last name
-int person_cmp(const void *p1, const void *p2);
+int person_cmp(const void *p1, const void *p2)
+{
+	Person *a = (Person *)p1;
+	Person *b = (Person *)p2;
+
+	if (a->age != b->age)
+		return b->age - a->age;
+
+	int first_name_cmp = strcmp(b->first_name, a->first_name);
+	if (first_name_cmp)
+		return first_name_cmp;
+
+	return strcmp(b->last_name, a->last_name);
+}
 
 // predicate: check if number is even
-int is_even(void *value);
+int is_even(void *value)
+{
+	int *el = (int *)value;
+	return *el % 2 ? 0 : 1;
+}
 
 // predicate: check if char is a vowel
-int is_vowel(void *value);
+int is_vowel(void *value)
+{
+	char *el = (char *)value;
+	switch (*el)
+	{
+	case 'a':
+	case 'e':
+	case 'i':
+	case 'o':
+	case 'u':
+		return 1;
+	default:
+		return 0;
+	}
+}
 
 // predicate: check if person is older than 25
-int is_older_than_25(void *person);
+int is_older_than_25(void *person)
+{
+	Person *el = (Person *)person;
+	return el->age > 25;
+}
 
 // -------------------------------------------------------------
 
-void read_int(void* value) {
-	scanf("%d", (int*)value);
+void read_int(void *value)
+{
+	scanf("%d", (int *)value);
 }
 
-void read_char(void* value) {
+void read_char(void *value)
+{
 	char c[2];
 	scanf("%s", c);
-	*(char*)value = c[0];
+	*(char *)value = c[0];
 }
 
-void read_person(void* value) {
-	Person *person = (Person*)value;
+void read_person(void *value)
+{
+	Person *person = (Person *)value;
 	scanf("%d %s %s", &person->age, person->first_name, person->last_name);
 }
 
-void vector_test(Vector *vector, int n, void(*read)(void*),
-		 int (*cmp)(const void*, const void*), int(*predicate)(void*)) {
+void vector_test(Vector *vector, int n, void (*read)(void *),
+				 int (*cmp)(const void *, const void *), int (*predicate)(void *))
+{
 	char op[2];
 	int index;
 	size_t size;
 	void *v = malloc(vector->element_size);
-	for (int i = 0; i < n; ++i) {
+	for (int i = 0; i < n; ++i)
+	{
 		scanf("%s", op);
-		switch (op[0]) {
-			case 'p': // push_back
-				read(v);
-				push_back(vector, v);
-				break;
-			case 'i': // insert
-				scanf("%d", &index);
-				read(v);
-				insert(vector, index, v);
-				break;
-			case 'e': // erase
-				scanf("%d", &index);
-				read(v);
-				erase(vector, index);
-				erase_value(vector, v, cmp);
-				break;
-			case 'd': // erase (predicate)
-				erase_if(vector, predicate);
-				break;
-			case 'r': // resize
-				scanf("%zu", &size);
-				resize(vector, size);
-				break;
-			case 'c': // clear
-				clear(vector);
-				break;
-			case 'f': // shrink
-				shrink_to_fit(vector);
-				break;
-			case 's': // sort
-				qsort(vector->data, vector->size,
-				      vector->element_size, cmp);
-				break;
-			default:
-				printf("No such operation: %s\n", op);
-				break;
+		switch (op[0])
+		{
+		case 'p': // push_back
+			read(v);
+			push_back(vector, v);
+			break;
+		case 'i': // insert
+			scanf("%d", &index);
+			read(v);
+			insert(vector, index, v);
+			break;
+		case 'e': // erase
+			scanf("%d", &index);
+			read(v);
+			erase(vector, index);
+			erase_value(vector, v, cmp);
+			break;
+		case 'd': // erase (predicate)
+			erase_if(vector, predicate);
+			break;
+		case 'r': // resize
+			scanf("%zu", &size);
+			resize(vector, size);
+			break;
+		case 'c': // clear
+			clear(vector);
+			break;
+		case 'f': // shrink
+			shrink_to_fit(vector);
+			break;
+		case 's': // sort
+			qsort(vector->data, vector->size,
+				  vector->element_size, cmp);
+			break;
+		default:
+			printf("No such operation: %s\n", op);
+			break;
 		}
 	}
 	free(v);
 }
 
-int main(void) {
+int main(void)
+{
 	int to_do, n;
 	Vector vector_int, vector_char, vector_person;
 
 	scanf("%d%d", &to_do, &n);
 
-	switch (to_do) {
-		case 1:
-			init_vector(&vector_int, 4, sizeof(int));
-			vector_test(&vector_int, n, read_int, int_cmp, is_even);
-			// print_vector_int(&vector_int);
-			// free(vector_int.data);
-			break;
-		// case 2:
-		// 	init_vector(&vector_char, 2, sizeof(char));
-		// 	vector_test(&vector_char, n, read_char, char_cmp, is_vowel);
-		// 	print_vector_char(&vector_char);
-		// 	free(vector_char.data);
-		// 	break;
-		// case 3:
-		// 	init_vector(&vector_person, 2, sizeof(Person));
-		// 	vector_test(&vector_person, n, read_person, person_cmp, is_older_than_25);
-		// 	print_vector_person(&vector_person);
-		// 	free(vector_person.data);
-		// 	break;
-		default:
-			printf("Nothing to do for %d\n", to_do);
-			break;
+	switch (to_do)
+	{
+	case 1:
+		init_vector(&vector_int, 4, sizeof(int));
+		vector_test(&vector_int, n, read_int, int_cmp, is_even);
+		print_vector_int(&vector_int);
+		free(vector_int.data);
+		break;
+	// case 2:
+	// 	init_vector(&vector_char, 2, sizeof(char));
+	// 	vector_test(&vector_char, n, read_char, char_cmp, is_vowel);
+	// 	print_vector_char(&vector_char);
+	// 	free(vector_char.data);
+	// 	break;
+	// case 3:
+	// 	init_vector(&vector_person, 2, sizeof(Person));
+	// 	vector_test(&vector_person, n, read_person, person_cmp, is_older_than_25);
+	// 	print_vector_person(&vector_person);
+	// 	free(vector_person.data);
+	// 	break;
+	default:
+		printf("Nothing to do for %d\n", to_do);
+		break;
 	}
 
 	return 0;
 }
-
